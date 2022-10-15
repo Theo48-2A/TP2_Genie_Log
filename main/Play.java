1a93d1e5fc4eb76ba59531b59f3efec14af23d2d
Theo48-2A <114155403+Theo48-2A@users.noreply.github.com>
2022-10-15 23:47:44 +0200

    Update Play.java

--- main/Play.java
+++ main/Play.java
@@ -1,4 +1,8 @@
 public class Play {
+  // Les variables static (finales aussi) qui correspondent au type de pièce qu'il est possible d'avoir  
+  public static final String comedy = "comedy";
+  public static final String tragedy = "tragedy";
+  //-----------------------------------------------------------------------------------------------
 
   public String name;
   public String type;
