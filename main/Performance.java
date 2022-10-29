43e54473c3198e962a42b2ee4bbf985f781ecbc1
Theo48-2A <114155403+Theo48-2A@users.noreply.github.com>
2022-10-29 11:45:07 +0200

    Update Performance.java

--- main/Performance.java
+++ main/Performance.java
@@ -1,10 +1,20 @@
 public class Performance {
 
-  public String playID;
-  public int audience;
+  private String playID;  //Identifiant performence
+  private int audience;   //Nombre de spectateurs
 
   public Performance(String playID, int audience) {
     this.playID = playID;
     this.audience = audience;
   }
+  
+  public String getPlayid(){
+     return this.playID;
+  }
+  
+  public int getAudience(){
+     return this.audience;
+  }
+  
+  
 }
