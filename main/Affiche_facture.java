d6fcdba6c10f6262fbef0f04d7586e3fcc2f25b3
Theo48-2A <114155403+Theo48-2A@users.noreply.github.com>
2022-10-29 11:47:17 +0200

    Update Affiche_facture.java

--- main/Affiche_facture.java
+++ main/Affiche_facture.java
@@ -6,5 +6,24 @@ import java.util.Objects;
 public class Affiche_facture{
 
    
+ private String contenu_Facture;     //Le contenu de la facture
+   private String nom_client;  		// Nom du client
+   private String[][] tableau_pieces_infos;    //Le tableau qui contient chaque pièce avec ses informations [[nom][nb_spectateurs][cout]]
+   private String[] tableau_fin_info;         //Le tableau qui contient le cout total ainsi que les points de fidelité gagnés
+
+   public Affiche_facture(String contenu_Facture, String nom_client, String[][] tableau_pieces_infos, String[] tableau_fin_info){
+      this.contenu_Facture = Objects.requireNonNull(contenu_Facture);
+      this.nom_client = Objects.requireNonNull(nom_client);
+      this.tableau_pieces_infos = Objects.requireNonNull(tableau_pieces_infos);
+      this.tableau_fin_info = Objects.requireNonNull(tableau_fin_info);
+
+   }
+
+
+
+
+
+
+
 
 }
