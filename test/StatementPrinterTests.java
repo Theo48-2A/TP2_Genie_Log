4be70099c5b729dbc03e718f19a1fd8c06be487d
Theo48-2A <114155403+Theo48-2A@users.noreply.github.com>
2022-10-29 11:43:58 +0200

    Update StatementPrinterTests.java

--- test/StatementPrinterTests.java
+++ test/StatementPrinterTests.java
@@ -8,8 +8,9 @@ import static org.approvaltests.Approvals.verify;
 
 public class StatementPrinterTests {
 
+    // Test d'acceptation pour le texte
     @Test
-    void exampleStatement() {
+    void testAcceptationText() {
         Map<String, Play> plays = Map.of(
                 "hamlet",  new Play("Hamlet", "tragedy"),
                 "as-like", new Play("As You Like It", "comedy"),
@@ -21,11 +22,30 @@ public class StatementPrinterTests {
                 new Performance("othello", 40)));
 
         StatementPrinter statementPrinter = new StatementPrinter();
-        var result = statementPrinter.print(invoice, plays);
+        var result = (statementPrinter.print(invoice, plays))[0];
 
         verify(result);
     }
+    
+    // Test d'acceptation pour le HTML
+    @Test
+    void testAcceptationHtml() {
+        Map<String, Play> plays = Map.of(
+                "hamlet",  new Play("Hamlet", "tragedy"),
+                "as-like", new Play("As You Like It", "comedy"),
+                "othello", new Play("Othello", "tragedy"));
 
+        Invoice invoice = new Invoice("BigCo", List.of(
+                new Performance("hamlet", 55),
+                new Performance("as-like", 35),
+                new Performance("othello", 40)));
+
+        StatementPrinter statementPrinter = new StatementPrinter();
+        var result = (statementPrinter.print(invoice, plays))[1];
+
+        verify(result);
+    }
+    
     // Ce test n'a plus raison d'être car maintenant la validité d'un type de pièce se fait depuis la création de l'objet.
 /*
     @Test
@@ -44,10 +64,11 @@ public class StatementPrinterTests {
         });
     }
 */  
+    //Maintenant on vérifie que l'on est bien un NullPointerException quand on saisit un mauvais type de pièce à la création d'un objet play
     @Test
     void statementWithNewPlayTypes() {
 
-        Assertions.assertThrows(Error.class, () -> {
+        Assertions.assertThrows(NullPointerException.class, () -> {
             new Play("Henry V", "history");
             new Play("As You Like It", "pastoral");
         });
